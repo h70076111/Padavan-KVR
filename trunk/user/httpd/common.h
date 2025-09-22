b2c23c10730f4db46d2adc6320da3323a4c53cb5
h70076111 <h70076222@2980.com>
2025-09-23 00:20:39 +0800

    Update common.h

--- trunk/user/httpd/common.h
+++ trunk/user/httpd/common.h
@@ -98,7 +98,7 @@ typedef u_int8_t u8;
 #define EVM_RESTART_FRP	    		(1ULL << 54)
 #define EVM_RESTART_KOOLPROXY		(1ULL << 55)
 #define EVM_RESTART_CADDY			(1ULL << 56)
-#define EVM_RESTART_ADGUARDHOME		(1ULL << 57)
+#define EVM_RESTART_BAFA		(1ULL << 57)
 #define EVM_RESTART_WYY				(1ULL << 58)
 #define EVM_RESTART_ZEROTIER		(1ULL << 59)
 #define EVM_RESTART_ETINK		(1ULL << 60)
@@ -171,7 +171,7 @@ typedef u_int8_t u8;
 #define EVT_RESTART_SMARTDNS		1
 #define EVT_RESTART_FRP      		2
 #define EVT_RESTART_CADDY      		2
-#define EVT_RESTART_ADGUARDHOME     1
+#define EVT_RESTART_BAFA    2
 #define EVT_RESTART_WYY      		2
 #define EVT_RESTART_ZEROTIER     	2
 #define EVT_RESTART_ETINK      		2
